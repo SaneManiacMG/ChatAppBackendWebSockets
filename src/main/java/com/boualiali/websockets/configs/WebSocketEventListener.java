1668e0562babeb6a88836ae40bbc63c21a3195bd
SaneManiacMG <mmoroane@hotmail.com>
2023-11-27 09:36:18 +0200

    Created ChatApp Backend

---
+++ src/main/java/com/boualiali/websockets/configs/WebSocketEventListener.java
@@ -0,0 +1,33 @@
+package com.boualiali.websockets.configs;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.boualiali.websockets.models.ChatMessage;
+import com.boualiali.websockets.models.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
+    
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            log.info("User disconnected {}", username);
+            ChatMessage message = new ChatMessage("User disconnected" + username, username, MessageType.LEAVE);
+            messageTemplate.convertAndSend("/topic/public", message);
+        }
+    }
+}
---
+++ src/main/java/com/boualiali/websockets/models/MessageType.java
@@ -0,0 +1,7 @@
+package com.boualiali.websockets.models;
+
+public enum MessageType {
+    CHAT, 
+    JOIN, 
+    LEAVE
+}
